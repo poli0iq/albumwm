3796cb41e1e3bd46380fec247126d3ab225ebbe0
Polina Vishneva <poli@0iq.dev>
2026-05-17 14:41:15 +0200

    wm: lib.js -> lib.ts

--- wm/lib.ts
+++ wm/lib.ts
@@ -4,13 +4,13 @@
  */
 
 /**
-   Find the first x in `values` that's larger than `cur`.
-   Cycle to first value if no larger value is found.
-   `values` should be sorted in ascending order.
+ * Find the first x in `values` that's larger than `cur`.
+ * Cycle to first value if no larger value is found.
+ * `values` should be sorted in ascending order.
  */
-export function findNext(cur, values, slack = 0) {
+export function findNext(cur: number, values: number[], slack = 0) {
     for (let i = 0; i < values.length; i++) {
-        let x = values[i];
+        const x = values[i];
         if (cur < x) {
             if (x - cur < slack) {
                 // Consider `cur` practically equal to `x`
@@ -23,10 +23,10 @@ export function findNext(cur, values, slack = 0) {
     return values[0]; // cycle
 }
 
-export function findPrev(cur, values, slack = 0) {
+export function findPrev(cur: number, values: number[], slack = 0) {
     let i = 0;
     for (; i < values.length; i++) {
-        let x = values[i];
+        const x = values[i];
         if (x + slack >= cur) {
             break;
         }
@@ -39,7 +39,7 @@ export function findPrev(cur, values, slack = 0) {
     return values[target];
 }
 
-export function arrayEqual(a, b) {
+export function arrayEqual<T extends ArrayLike<unknown>>(a: T, b: T) {
     if (a === b) return true;
     if (!a || !b) return false;
     if (a.length !== b.length) return false;
@@ -49,22 +49,22 @@ export function arrayEqual(a, b) {
     return true;
 }
 
-/** Is the floating point numbers equal enough */
-export function eq(a, b, epsilon = 0.00000001) {
+/** Are the floating point numbers equal enough */
+export function eq(a: number, b: number, epsilon = 0.00000001) {
     return Math.abs(a - b) < epsilon;
 }
 
-export function swap(array, i, j) {
-    let temp = array[i];
+export function swap<T>(array: T[], i: number, j: number) {
+    const temp = array[i];
     array[i] = array[j];
     array[j] = temp;
 }
 
-export function inBounds(array, i) {
+export function inBounds<T extends ArrayLike<unknown>>(array: T, i: number) {
     return i >= 0 && i < array.length;
 }
 
-export function indent(level, str) {
+export function indent(level: number, str: string) {
     let blank = '';
     for (let i = 0; i < level; i++) {
         blank += '  ';
@@ -72,13 +72,13 @@ export function indent(level, str) {
     return blank + str;
 }
 
-export function sum(array) {
+export function sum(array: number[]) {
     return array.reduce((a, b) => a + b, 0);
 }
 
-export function zip(...as) {
-    let r = [];
-    let minLength = Math.min(...as.map(x => x.length));
+export function zip<T>(...as: T[][]) {
+    const r = [];
+    const minLength = Math.min(...as.map(x => x.length));
     for (let i = 0; i < minLength; i++) {
         r.push(as.map(a => a[i]));
     }
