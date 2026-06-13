8201d76e839a45ae3524aa40377b9c7d24126bfe
Polina Vishneva <poli@0iq.dev>
2026-06-13 22:19:23 +0200

    ui/WinpropsRow: Port to Adwaita widgets

--- preferences/winpropsPage.ts
+++ preferences/winpropsPage.ts
@@ -5,7 +5,9 @@ import Gtk from 'gi://Gtk';
 
 import type { WinPropSpec } from '../wm/settings.js';
 
-export class WinpropsRow extends Gtk.ListBoxRow {
+const _ = (s: string) => s;
+
+export class WinpropsRow extends Adw.ExpanderRow {
     static {
         GObject.registerClass(
             {
@@ -16,18 +18,15 @@ export class WinpropsRow extends Gtk.ListBoxRow {
                     GLib.UriFlags.NONE
                 ),
                 InternalChildren: [
-                    'header',
-                    'descLabel',
-                    'accelLabel',
-                    'revealer',
-                    'optionList',
-                    'wmClass',
+                    'accel_revealer',
+                    'accel_label',
+                    'wm_class',
                     'title',
-                    'scratchLayer',
-                    'preferredWidth',
+                    'scratch_layer',
+                    'preferred_width',
                     'space',
                     'focus',
-                    'deleteButton',
+                    'delete_button',
                 ],
                 Properties: {
                     winprop: GObject.ParamSpec.jsobject(
@@ -36,13 +35,6 @@ export class WinpropsRow extends Gtk.ListBoxRow {
                         'Winprop',
                         GObject.ParamFlags.READWRITE |
                             GObject.ParamFlags.CONSTRUCT_ONLY
-                    ),
-                    expanded: GObject.ParamSpec.boolean(
-                        'expanded',
-                        'Expanded',
-                        'Expanded',
-                        GObject.ParamFlags.READWRITE,
-                        false
                     ),
                 },
                 Signals: {
@@ -56,18 +48,14 @@ export class WinpropsRow extends Gtk.ListBoxRow {
 
     declare winprop: WinPropSpec;
 
-    declare _wmClass: Gtk.Entry;
-    declare _title: Gtk.Entry;
-    declare _scratchLayer: Gtk.Switch;
-    declare _preferredWidth: Gtk.Entry;
-    declare _space: Gtk.ComboBoxText;
-    declare _focus: Gtk.Switch;
-
-    declare _descLabel: Gtk.Label;
-    declare _accelLabel: Gtk.Label;
-    declare _revealer: Gtk.Revealer;
-
-    _expanded: boolean = false;
+    declare _accel_revealer: Gtk.Revealer;
+    declare _accel_label: Gtk.Label;
+    declare _wm_class: Adw.EntryRow;
+    declare _title: Adw.EntryRow;
+    declare _scratch_layer: Adw.SwitchRow;
+    declare _preferred_width: Adw.EntryRow;
+    declare _space: Adw.ComboRow;
+    declare _focus: Adw.SwitchRow;
 
     constructor(props: Partial<WinpropsRow.ConstructorProps>) {
         super(props);
@@ -80,11 +68,11 @@ export class WinpropsRow extends Gtk.ListBoxRow {
         this._setDescLabel();
 
         // set the values to current state and connect to 'changed' signal
-        this._wmClass.set_text(this.winprop.wm_class ?? '');
-        this._wmClass.connect('changed', () => {
+        this._wm_class.set_text(this.winprop.wm_class ?? '');
+        this._wm_class.connect('changed', () => {
             // check if null or empty (we still emit changed if wm_class is wiped)
             this.checkHasWmClassOrTitle();
-            this.winprop.wm_class = this._wmClass.get_text();
+            this.winprop.wm_class = this._wm_class.get_text();
             this._setDescLabel();
             this.emit('changed');
         });
@@ -97,60 +85,62 @@ export class WinpropsRow extends Gtk.ListBoxRow {
             this.emit('changed');
         });
 
-        this._scratchLayer.set_active(this.winprop.scratch_layer ?? false);
-        this._scratchLayer.connect('state-set', () => {
-            const isActive = this._scratchLayer.get_active();
+        this._scratch_layer.set_active(this.winprop.scratch_layer ?? false);
+        this._scratch_layer.connect('notify::active', () => {
+            const isActive = this._scratch_layer.get_active();
             this.winprop.scratch_layer = isActive;
 
             // if is active then disable the preferredWidth input
-            this._preferredWidth.set_sensitive(!isActive);
+            this._preferred_width.set_sensitive(!isActive);
 
             this.emit('changed');
         });
 
-        this._preferredWidth.set_text(this.winprop.preferredWidth ?? '');
+        this._preferred_width.set_text(this.winprop.preferredWidth ?? '');
         // if scratchLayer is active then users can't edit preferredWidth
-        this._preferredWidth.set_sensitive(
+        this._preferred_width.set_sensitive(
             !(this.winprop.scratch_layer ?? false)
         );
 
-        this._preferredWidth.connect('changed', () => {
+        this._preferred_width.connect('changed', () => {
             // if has value, needs to be valid (have a value or unit)
-            if (this._preferredWidth.get_text()) {
-                const value = this._preferredWidth.get_text();
+            if (this._preferred_width.get_text()) {
+                const value = this._preferred_width.get_text();
                 const digits = (value.match(/\d+/) ?? [null])[0];
                 const isPercent = /^.*%$/.test(value);
                 const isPixel = /^.*px$/.test(value);
 
                 // check had valid number
                 if (!digits) {
-                    this._setError(this._preferredWidth);
+                    this._setError(this._preferred_width);
                 }
                 // if no unit defined
                 else if (!isPercent && !isPixel) {
-                    this._setError(this._preferredWidth);
+                    this._setError(this._preferred_width);
                 } else {
-                    this._setError(this._preferredWidth, false);
+                    this._setError(this._preferred_width, false);
                     this.winprop.preferredWidth =
-                        this._preferredWidth.get_text();
+                        this._preferred_width.get_text();
                     this.emit('changed');
                 }
             } else {
                 // having no preferredWidth is valid
-                this._setError(this._preferredWidth, false);
+                this._setError(this._preferred_width, false);
                 delete this.winprop.preferredWidth;
                 this.emit('changed');
             }
         });
 
-        this._space.append_text('CURRENT');
+        const workspaceList = new Gtk.StringList();
+        workspaceList.append('Currently active');
         for (let i = 0; i < 16; i++) {
-            this._space.append_text(`Workspace ${i + 1}`);
+            workspaceList.append(`Workspace ${i + 1}`);
         }
+        this._space.set_model(workspaceList);
         // index 0 is CURRENT, so add 1
-        this._space.set_active((this.winprop.spaceIndex ?? -1) + 1);
-        this._space.connect('changed', () => {
-            let value: number | undefined = this._space.get_active() - 1;
+        this._space.set_selected((this.winprop.spaceIndex ?? -1) + 1);
+        this._space.connect('notify::selected', () => {
+            let value: number | undefined = this._space.get_selected() - 1;
             if (value < 0) {
                 value = undefined;
             }
@@ -159,26 +149,27 @@ export class WinpropsRow extends Gtk.ListBoxRow {
         });
 
         this._focus.set_active(this.winprop.focus ?? true);
-        this._focus.connect('state-set', () => {
+        this._focus.connect('notify::active', () => {
             const isActive = this._focus.get_active();
             this.winprop.focus = isActive;
             this.emit('changed');
         });
 
+        this.connect('notify::expanded', this._updateState.bind(this));
         this._updateState();
     }
 
     /**
-     * Checks has an input for either wmClass or title.
+     * Checks has an input for either wm_class or title.
      * Sets 'error' cssClass is neither.
      */
     checkHasWmClassOrTitle() {
-        if (!this._wmClass.get_text() && !this._title.get_text()) {
-            this._setError(this._wmClass);
+        if (!this._wm_class.get_text() && !this._title.get_text()) {
+            this._setError(this._wm_class);
             this._setError(this._title);
             return false;
         } else {
-            this._setError(this._wmClass, false);
+            this._setError(this._wm_class, false);
             this._setError(this._title, false);
             return true;
         }
@@ -205,18 +196,6 @@ export class WinpropsRow extends Gtk.ListBoxRow {
         }
     }
 
-    get expanded() {
-        return this._expanded;
-    }
-
-    set expanded(value) {
-        if (this._expanded === value) return;
-
-        this._expanded = value;
-        this.notify('expanded');
-        this._updateState();
-    }
-
     _onDeleteButtonClicked() {
         this.emit('row-deleted');
     }
@@ -239,32 +218,22 @@ export class WinpropsRow extends Gtk.ListBoxRow {
     _setDescLabel() {
         // if wmClass, use that, otherwise use title (fallback)
         if (this.winprop.wm_class) {
-            this._descLabel.label = this.winprop.wm_class;
+            this.set_title(this.winprop.wm_class);
         } else if (this.winprop.title) {
-            this._descLabel.label = this.winprop.title;
+            this.set_title(this.winprop.title);
+        } else {
+            this.set_title(_('New window property'));
         }
     }
 
     _updateState() {
-        GLib.idle_add(0, () => {
-            this._accelLabel.label = this._setAccelLabel();
-            if (this.expanded) {
-                this._accelLabel.hide();
-                this._revealer.reveal_child = true;
-                this.add_css_class('expanded');
-            } else {
-                this._accelLabel.show();
-                this._revealer.reveal_child = false;
-                this.remove_css_class('expanded');
-            }
-
-            return GLib.SOURCE_REMOVE;
-        });
+        this._accel_label.label = this._setAccelLabel();
+        this._accel_revealer.set_reveal_child(!this.expanded);
     }
 }
 
 export declare namespace WinpropsRow {
-    interface ConstructorProps extends Gtk.ListBoxRow.ConstructorProps {
+    interface ConstructorProps extends Adw.ExpanderRow.ConstructorProps {
         winprop: WinPropSpec;
     }
 }
@@ -279,7 +248,7 @@ export class WinpropsPage extends Adw.PreferencesPage {
                     '../ui/WinpropsPage.ui',
                     GLib.UriFlags.NONE
                 ),
-                InternalChildren: ['listbox', 'addButton'],
+                InternalChildren: ['winprops', 'add_button'],
                 Signals: {
                     changed: {},
                 },
@@ -288,10 +257,10 @@ export class WinpropsPage extends Adw.PreferencesPage {
         );
     }
 
+    declare _winprops: Adw.PreferencesGroup;
+
     declare rows: WinpropsRow[];
     declare _expandedRow: WinpropsRow | null;
-
-    declare _listbox: Gtk.ListBox;
 
     _init(params = {}) {
         super._init(params);
@@ -302,12 +271,12 @@ export class WinpropsPage extends Adw.PreferencesPage {
 
     addWinprops(winprops: WinPropSpec[]) {
         winprops.forEach(winprop => {
-            this._listbox.insert(this._createRow(winprop), -1);
+            this._winprops.add(this._createRow(winprop));
         });
     }
 
     _removeRow(row: WinpropsRow) {
-        this._listbox.remove(row);
+        this._winprops.remove(row);
         const remove = this.rows.findIndex(r => r === row);
         if (remove >= 0) {
             this.rows.splice(remove, 1);
@@ -318,7 +287,7 @@ export class WinpropsPage extends Adw.PreferencesPage {
     _onAddButtonClicked() {
         const row = this._createRow();
         row.expanded = true;
-        this._listbox.insert(row, 0);
+        this._winprops.add(row);
     }
 
     _createRow(winprop?: WinPropSpec) {
@@ -329,13 +298,6 @@ export class WinpropsPage extends Adw.PreferencesPage {
         row.connect('row-deleted', () => this._removeRow(row));
         row.connect('changed', () => this.emit('changed'));
         return row;
-    }
-
-    _onRowActivated(_list: Gtk.ListBox, row: WinpropsRow) {
-        if (!row.is_focus()) {
-            return;
-        }
-        row.expanded = !row.expanded;
     }
 
     _onRowExpanded(row: WinpropsRow) {
